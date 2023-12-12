51784885161d05fd8285ed723a75f101b2dbf331
eugenek92 <eugenek9218@gmail.com>
2023-12-12 11:03:11 +0200

    Release version

---
+++ Cpp_OOP_Exam/CoffeeShop.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Admin.h"
+#include <iostream>
+#include <string>
+#include "Admin.h"
+#include "Client.h"
+#include <fstream>
+
+using namespace std;
+
+void Logo();
+
+class CoffeeShop
+{
+
+public:
+	void Menu();
+};
+
--- Cpp_OOP_Exam/Source.cpp
+++ Cpp_OOP_Exam/Source.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-
+#include "CoffeeShop.h"
 using namespace std;
-
-void Menu();
+//void Menu();
 
 int main() {
 	
-	Menu();
-
+	//Menu();
+	CoffeeShop shop;
+	
+	shop.Menu();
 	return 0;
 
 }
